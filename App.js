81e09ff5195735ebd7ca61dde54cf689cfd39a39
Bruce Crevensten <bruce.crevensten@gmail.com>
2017-11-02 19:08:05 -0700

    remove unused dev instructions

--- App.js
+++ App.js
@@ -9,17 +9,6 @@ import {
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
- // 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-  
-
-
 //export default class App extends Component<{}> {
 class WeatherScreen extends Component {
   static navigationOptions = {
